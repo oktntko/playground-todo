3d8b346f4f52705695ea890a2d2bb4be24773c78
oktntko <oktntko@gmail.com>
2023-05-15 00:25:16 +0900

    バックエンド接続

--- 03.web-front/src/pages/index.vue
+++ 03.web-front/src/pages/index.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+import axios from "axios";
+import { onMounted } from "vue";
+
+type Todo = {
+  id: number;
+  yarukoto: string;
+  datetime: Date;
+};
+
+onMounted(async () => {
+  const res = await axios.get<Todo[]>("/api/todo");
+
+  console.log(res);
+  console.log(res.data);
+});
+</script>
+
 <template>
   <div>Hello World!</div>
 </template>
