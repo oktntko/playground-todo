e6c787223d80917b2caad83f5fb5ea363d4fb89f
oktntko <oktntko@gmail.com>
2023-06-03 23:24:09 +0900

    wip

--- 01.console-app/todo-app/04.try-database/x02_INSERT.sql
+++ 01.console-app/todo-app/04.try-database/x02_INSERT.sql
@@ -1,13 +1,13 @@
 -- https://www.postgresql.jp/document/14/html/tutorial-populate.html
 -- 2.4. テーブルに行を挿入
 INSERT
-INTO todo(yarukoto, datetime)
-VALUES ('電話をかける', '2023-04-01 10:23:45.678');
+INTO todo(yarukoto, kizitu)
+VALUES ('電話をかける', '2023-04-01');
 
 INSERT
-INTO todo(yarukoto, datetime)
-VALUES ('カレンダーを見る', '2023-04-02 10:23:45.678');
+INTO todo(yarukoto, kizitu)
+VALUES ('カレンダーを見る', '2023-04-02');
 
 INSERT
-INTO todo(yarukoto, datetime)
-VALUES ('テレビを見る', '2023-04-02 11:23:45.678');
+INTO todo(yarukoto, kizitu)
+VALUES ('テレビを見る', '2023-04-02');
